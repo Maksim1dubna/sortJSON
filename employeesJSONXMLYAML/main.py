bc3e4423507dc8a4663b50474c0f0269db6953be
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-07-15 14:07:54 +0300

    Add files via upload

---
+++ employeesJSONXMLYAML/main.py
@@ -0,0 +1,15 @@
+import json
+from operator import itemgetter
+def employees_rewrite(sort_type):
+    with open('employees.json', "r") as json_file:
+        data = json.load(json_file)
+        switch = False
+        if type(data['employees'][0][sort_type]) is int:
+            switch = True
+        data['employees'] = sorted(data['employees'], key=lambda x: x[sort_type], reverse=switch)
+
+        with open('employees_lastname_sorted.json', 'w', encoding='utf-8') as json_file_write:
+            json.dump(data, json_file_write, ensure_ascii=False,indent=4)
+        return data
+print(employees_rewrite('salary'))
+
